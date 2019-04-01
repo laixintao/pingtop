833d17fbf056d27837ca8da490db087b76f913b1
laixintao <nanshi.lxt@alipay.com>
2019-04-01 22:39:55 +0800

    setup project.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="pingtop",
+    version="0.0.1",
+    description="Top like ping tool.",
+    author="laixintao",
+    author_email="laixintaoo@gmail.com",
+    url="https://github.com/laixintao/pingtop",
+)
