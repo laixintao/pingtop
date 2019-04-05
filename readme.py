3bc1c7e9cfef13d9d2e0e392c46a74b96c03f42d
laixintao <nanshi.lxt@alipay.com>
2019-04-05 11:28:38 +0800

    add Column list readme

---
+++ readme.py
@@ -0,0 +1,11 @@
+
+Column list:
+- host: host name
+- ip: real ip
+- minrtt: min round trip time
+- maxrtt: max round trip time
+- artt: average round trip time
+- lost_p: lost package percentages
+- lost: lost package count
+- seq: icmq_seq (aka, the total sent packages)
+- ttl: 
