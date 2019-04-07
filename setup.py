93669c7dc928dcf93ede033045ed5c6dc929368c
laixintao <laixintao1995@163.com>
2019-04-07 23:59:00 +0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="pingtop",
-    version="0.1.0",
+    version="0.1.1",
     description="Top like ping tool.",
     author="laixintao",
     author_email="laixintaoo@gmail.com",
