c8690f978de3e1a9b633c0cf0fd8122f62be7145
jason <jasonqiao36@gmail.com>
2019-04-09 23:53:00 +0800

    package project with readme file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with open(path.join(this_directory, "readme.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
