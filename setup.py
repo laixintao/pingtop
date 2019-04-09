7b9479f33e4d411c916451f09931133fe9826e57
laixintao <nanshi.lxt@alipay.com>
2019-04-09 22:21:27 +0800

    resolve dependency

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     author_email="laixintaoo@gmail.com",
     url="https://github.com/laixintao/pingtop",
     entry_points={"console_scripts": ["pingtop = pingtop:multi_ping"]},
-    scripts=['pingtop.py'],
+    scripts=["pingtop.py"],
+    install_requires=["panwid", "click"],
 )
