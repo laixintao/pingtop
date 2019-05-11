3f2f0ef10fba323af4d7064786454b75345dfe55
laixintao <laixintao1995@163.com>
2019-05-11 10:29:03 +0800

    bugfix: forget to update setup.py
    ref: https://github.com/laixintao/pingtop/pull/25

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "readme.md")) as f:
+with open(path.join(this_directory, "README.rst")) as f:
     long_description = f.read()
 
 setup(
