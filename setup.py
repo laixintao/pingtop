d6b442f414cbe2a6a7e0d697c7b146c529407f34
laixintao <laixintao1995@163.com>
2019-05-25 20:35:56 +0800

    Bump version: 0.2.11 → 0.2.12

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.rst")) as f:
 
 setup(
     name="pingtop",
-    version="0.2.11",
+    version="0.2.12",
     py_modules=["ping"],
     description="Ping multiple servers and show the result in a top like terminal UI.",
     author="laixintao",
