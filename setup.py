e7638e90aa2d3fa40d7f191837a9c6fe45285bcd
laixintao <laixintao1995@163.com>
2019-08-08 23:17:03 +0800

    no need to add scripts

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     author_email="laixintaoo@gmail.com",
     url="https://github.com/laixintao/pingtop",
     entry_points={"console_scripts": ["pingtop = pingtop:multi_ping"]},
-    scripts=["pingtop.py"],
     install_requires=["panwid", "click"],
     classifiers=[
         "Programming Language :: Python",
